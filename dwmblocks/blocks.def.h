7ab385e4b0b905e9394425a9081a3e356423d3b9
gentoo <gen@too.com>
2021-07-28 00:09:31 -0400

    bar height patch

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"", "cat /sys/class/thermal/thermal_zone0/temp | sed 's/000/°C/'"0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"  ", "~/.dotfiles/scripts/temp.sh", 2 , 0},
+	{" ", "~/.dotfiles/scripts/cpuload.sh", 2 , 0},
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g ",	5, 0},
+	{" ", "~/.dotfiles/scripts/hdd.sh", 40, 0},
+	{" ", "iwgetid -r" , 1, 0},
+//	{"", "acpi |cut -d , -f 2", 5, 0},
+//	{" ", "/usr/bin/checkupdates| wc -l ", 1500, 0},
+	{" ", "~/.dotfiles/scripts/batt.sh ", 5, 0},
+	{" ", "pulsemixer --get-volume|cut -d \" \" -f 1", 2, 0},
+	{"", "date '+%I:%M%p' ", 5, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
