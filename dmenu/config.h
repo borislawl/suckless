07cc62d72c73b26d36f988e0955ed3d102202eda
me <me@w530>
2022-06-18 21:57:30 -0400

    a

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
-static const unsigned int alpha = 0xf0;
+static const unsigned int alpha = OPAQUE;
 static unsigned int border_width = 0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
